0f709ded6e13e73976467064455c8e0351eefdc6
Rizky Noor Ichwanudin <rizky.noor.ichwanudin@gmail.com>
2023-09-19 10:35:54 +0700

    update

--- soal-1.js
+++ soal-1.js
@@ -1,4 +1,13 @@
-const tanggal = new Date("2023-09-19");
-console.log(tanggal);
-console.log(tanggal);
-console.log(tanggal);
+const getDay = (date) => {
+    // console.log(tanggal);
+    let getDay = tanggal.getDay();
+    console.log(getDay);
+    if (getDay === 0 || getDay === 1) {
+        return `${date} merupakan Weekend`;
+    } else {
+        return `${date} bukan Weekend`;
+    }
+}
+
+const tanggal = new Date("2023-09-10");
+console.log(getDay(tanggal));

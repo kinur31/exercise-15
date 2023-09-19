ebeaf8d008eeba57eef31fff100e6dd9b4534e6e
yoelwildanto <yoel.wildantop@gmail.com>
2023-09-19 14:37:46 +0700

    final

--- soal-1.js
+++ soal-1.js
@@ -1,6 +1,5 @@
 const getDay = (date) => {
     let getDay = new Date(date).getDay();
-    console.log(getDay);
     if (getDay === 0 || getDay === 1) {
         return `${date} merupakan Weekend`;
     } else {

49073fa40eed6cf7900eb6538bdea69f688dd645
yoelwildanto <yoel.wildantop@gmail.com>
2023-09-19 13:10:01 +0700

    9-12 done

---
+++ index.js
@@ -0,0 +1,38 @@
+const {copyFirstHalf, dayInMonth, hariBerlalu, calculateAge} = require ("./soal9-12")
+
+
+
+//soal 1
+
+//soal 2
+
+//soal 3
+
+//soal 4
+
+//soal 5
+
+//soal 6
+
+//soal 7
+
+//soal 8
+
+//soal 9
+const number = [1,2,3,4,5]
+const result9 = copyFirstHalf(number)
+console.log(result9)
+
+//soal 10
+const tahun = 2023;
+const bulan = 1;
+const result10 = dayInMonth(bulan,tahun)
+console.log(result10)
+
+//soal 11
+console.log(hariBerlalu())
+
+//soal 12
+const tanggalLahir = "2000-1-1"
+const result12 = calculateAge(tanggalLahir)
+console.log(result12)
